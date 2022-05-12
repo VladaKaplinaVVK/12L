6322b291a024e08e21a1eed5e491fce177f9acd5
VladakaplinaVVK <vlada-kaplina-vvk@mail.ru>
2022-05-13 00:43:22 +0300

    12 lesson

--- loader/views.py
+++ loader/views.py
@@ -1,6 +1,9 @@
-from os import abort
+
 from flask import Blueprint, render_template, send_from_directory, request, json
 import logging
+
+from flask import abort
+
 from exceptions import WrongImgType
 from loader.utils import save_picture, add_post
 from config import POST_PATH
@@ -18,8 +21,8 @@ def create_new_post():
 
 @loader_blueprint.route("/post", methods=['POST'])
 def create_new_post_by_user(picture_path=None):
-    picture = request.args.get("picture")
-    content = request.args.get("content")
+    picture = request.files.get("picture")
+    content = request.form.get("content")
     if not picture or not content:
         logging.info("Данные не загружены, отсутсвует часnь данных")
         return "Отсутствует часть данных"
